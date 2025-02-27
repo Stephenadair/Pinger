d72841c3050cf814de0ca2de0da2bc819313bdf2
Stephen Adair <adairs2@winthrop.edu>
2025-02-27 13:45:15 -0500

    added little bit of cypod1

--- pinger.cpp
+++ pinger.cpp
@@ -9,9 +9,27 @@ bool ping(string ip) {
 }
 
 int main() {
+
+    string fw_out_cypod1 = "192.168.1.1";
+    string fw_in_cypod1 = "172.21.1.254";
+
+    if (ping(fw_out_cypod1)){
+        cout << "Successfully pinged outside firewall of cypod 1" << endl;
+        if (ping(fw_in_cypod1)){
+            cout << "Successfully pinged router of cypod 1" << endl;
+        }
+        else {
+            cout << "Ping to " << fw_in_cypod1 << " failed.\n";
+        }
+    }
+    else {
+        cout << "Ping to " << fw_out_cypod1 << " failed.\n";
+    }
+
+
+
     int temp = 26;  // Number of IPs to test
     string junk;
-
     for (int i = 11; i < temp; i++) {
         junk = "192.168.1." + to_string(i); // Convert int to string
 
