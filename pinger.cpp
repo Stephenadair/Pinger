ccd807ac8f5bf78711b85039702bba43716a7f20
Stephen Adair <adairs2@winthrop.edu>
2025-02-27 12:23:24 -0500

    Pinged first 15 ip addreses

--- pinger.cpp
+++ pinger.cpp
@@ -1,1 +1,26 @@
-// pinger program
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+bool ping(string ip) {
+    string command = "ping -c 1 " + ip + " > /dev/null 2>&1"; // Linux/macOS
+    return system(command.c_str()) == 0;
+}
+
+int main() {
+    int temp = 26;  // Number of IPs to test
+    string junk;
+
+    for (int i = 11; i < temp; i++) {
+        junk = "192.168.1." + to_string(i); // Convert int to string
+
+        if (ping(junk)) {
+            cout << "Ping to " << junk << " was successful!\n";
+        } else {
+            cout << "Ping to " << junk << " failed.\n";
+        }
+    }
+
+    return 0;
+}
