df613eb0c597e61abd4b9c888e550d56be1a669d
Stephen Adair <adairs2@winthrop.edu>
2025-03-04 12:04:00 -0500

    Successful ping of workstations and putting in log file

--- Pinger.sh
+++ Pinger.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-if ["ping -c 1 192.168.1.20"]
-then 
-    echo "Pinged cybernet 10"
-else 
-        echo "error"
-fi
+
+log_file="/home/stephen/Documents/Pinger/Log.txt"
+
+> "$log_file"
+
+for i in {11..25}; do 
+    if ping -c 1 -W 1 192.168.1.$i >> "$log_file" 2>&1;
+    then 
+        echo "Pinged cybernet $i" | tee -a "$log_file"
+    else 
+        echo "error" | tee -a "$log_file"
+    fi
+done
